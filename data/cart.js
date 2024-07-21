6e15f9349f374083e62fa85a8ad9ea5ba7aa1ce3
UdayKiran <kiranuday1730@gmail.com>
2024-07-21 10:11:29 +0530

    Made checkout page intractive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productId:'3ebe75dc-64d2-4137-8860-1f5a963e534b',
+  quantity:1
+},{
+  productId:'8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
+  quantity:1
+}];
 
 export function addToCart(addToCartElement){
   let matchingItem;
