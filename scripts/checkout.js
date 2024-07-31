4c36c3df12d54e33183eb7dda46f6e211345a7c2
UdayKiran <kiranuday1730@gmail.com>
2024-07-31 20:25:49 +0530

    created oops version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderCheckoutPage} from './checkout/orderSummery.js';
 import {renderPaymentDetails} from './checkout/paymentSummery.js';
+import '../data/cart-oops.js';
 
 renderCheckoutPage();
 renderPaymentDetails();
