df7cccc7c51477f967473bc7758268bff38532ab
UdayKiran <kiranuday1730@gmail.com>
2024-08-01 17:29:57 +0530

    created class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderCheckoutPage} from './checkout/orderSummery.js';
 import {renderPaymentDetails} from './checkout/paymentSummery.js';
-import '../data/cart-oops.js';
+import '../data/cart-class.js';
 
 renderCheckoutPage();
 renderPaymentDetails();
