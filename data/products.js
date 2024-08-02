0fb9a4ec1124afb83a5f9f72c3e22e591802556b
UdayKiran <kiranuday1730@gmail.com>
2024-08-02 20:42:32 +0530

    loaded products from backend

--- data/products.js
+++ data/products.js
@@ -54,8 +54,30 @@ export class Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(callbackFunction){
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load',()=>{
+    products = JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      else{
+        return new Product(productDetails);
+      }
+    });
+    callbackFunction();
+  });
+
+  xhr.open('GET','https://supersimplebackend.dev/products');
+  xhr.send();
+
+}
 
 
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -723,4 +745,4 @@ export const products = [
     return new Product(productDetails);
   }
 });
-
+*/
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import {renderCheckoutPage} from './checkout/orderSummery.js';
 import {renderPaymentDetails} from './checkout/paymentSummery.js';
+import { loadProducts } from '../data/products.js';
 import '../data/cart-class.js';
 import '../data/backend-practice.js';
 
-renderCheckoutPage();
-renderPaymentDetails();
+loadProducts(()=>{
+  renderCheckoutPage();
+  renderPaymentDetails();
+});
