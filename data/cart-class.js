56fe3a17816fbe67cc8f8bd30bfcc5071b6fe0eb
UdayKiran <kiranuday1730@gmail.com>
2024-08-02 19:16:01 +0530

    add backed-practice file

--- data/cart-class.js
+++ data/cart-class.js
@@ -73,9 +73,6 @@ const businessCart = new Cart('cart-business');
 businessCart.addToCart('dd82ca78-a18b-4e2a-9250-31e67412f98d');
 
 
-console.log(cart);
-console.log(businessCart);
-
 /*export let cart;
 loadFromStorage();
 export function loadFromStorage(){
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderCheckoutPage} from './checkout/orderSummery.js';
 import {renderPaymentDetails} from './checkout/paymentSummery.js';
 import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderCheckoutPage();
 renderPaymentDetails();
