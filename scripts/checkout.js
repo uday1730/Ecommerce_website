aabda512b4eb480cdd35e7912c1f50be102f2a5c
UdayKiran <kiranuday1730@gmail.com>
2024-08-04 16:04:46 +0530

    converted promises to asyncawait

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,19 @@ import {loadCart} from '../data/cart.js';
 import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+async function loadPage(){
+
+   await loadProductsFetch();
+
+   await new Promise((resolve)=>{
+    loadCart(()=>{resolve();});
+   });
+   
+   renderCheckoutPage();
+   renderPaymentDetails();  
+}
+loadPage().then(()=>{console.log('Displayed')});
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve)=>{
@@ -16,6 +29,7 @@ Promise.all([
   renderCheckoutPage();
   renderPaymentDetails();
 });
+*/
 
 /*Using call-back which increases nesting of code
 loadProducts(()=>{
