53784ffddca717a060120756c10f03544e59ce8e
UdayKiran <kiranuday1730@gmail.com>
2024-08-04 18:18:32 +0530

    cart id issue solved with renaming with id

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderCheckoutPage} from './checkout/orderSummery.js';
 import {renderPaymentDetails} from './checkout/paymentSummery.js';
-import {loadProducts,loadProductsFetch} from '../data/products.js';
+import {loadProducts,loadProductsFetch, products} from '../data/products.js';
 import {loadCart} from '../data/cart.js';
 import '../data/cart-class.js';
 //import '../data/backend-practice.js';
@@ -9,7 +9,6 @@ async function loadPage(){
   try{
     //throw 'error';
     await loadProductsFetch();
-
     await new Promise((resolve,reject)=>{
       //throw 'error2';
      loadCart(()=>{
